9a3c6bb14f48c5e7a1d07d21cfb4238f9c36e70d
Eugene Molotov <molotov@it-projects.info>
2021-03-17 12:00:49 +0500

    :arrow_up: :one::two: portal_passport_editing

--- portal_passport_editing/controllers/controllers.py
+++ portal_passport_editing/controllers/controllers.py
@@ -2,9 +2,9 @@
 # Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo.addons.website_portal.controllers.main import website_account
+from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
-class website_account_passport(website_account):
+class website_account_passport(CustomerPortal):
 
-    OPTIONAL_BILLING_FIELDS = website_account.OPTIONAL_BILLING_FIELDS + ["passport", "birthdate_date", "nationality_id"]
+    OPTIONAL_BILLING_FIELDS = CustomerPortal.OPTIONAL_BILLING_FIELDS + ["passport", "birthdate_date", "nationality_id"]
