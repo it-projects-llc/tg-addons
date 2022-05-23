7e11c59059f391404eb3ef87d5fc82e1cc1f3127
Eugene Molotov <molotov@it-projects.info>
2022-05-23 15:36:32 +0500

    [ADD] base_vat_panama: модуль для клиента
    Co-authored-by: Alexandr Kolushov <KolushovAlexandr@users.noreply.github.com>
    Co-authored-by: Victor Bykov <BykovVik@users.noreply.github.com>
    Co-authored-by: Eugene Molotov <em230418@users.noreply.github.com>

---
+++ base_vat_panama/models/models.py
@@ -0,0 +1,25 @@
+# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    ruc_vat_panama = fields.Char("RUC")
+    dv_vat_panama = fields.Char("DV")
+
+    def _split_vat(self, vat):
+        vat_country = vat[:3].lower()
+        # Panamanian VaT numbers start with 'RUC'
+        if vat_country == "ruc":
+            return "pacustom", vat
+        return super(ResPartner, self)._split_vat(vat)
+
+    def check_vat_pacustom(self, vat):
+        return True
+
+    @api.model
+    def simple_vat_check(self, country_code, vat_number):
+        return True
