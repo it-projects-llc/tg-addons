cd3d4f8056a87c513039cb139f5b9dc97995fc94
Eugene Molotov <molotov@it-projects.info>
2023-02-21 19:48:49 +0500

    [ADD] tg_auth_signup: добавил модуль для клиента

---
+++ tg_auth_signup/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": """Signup modifications for Tribal Gathering""",
+    "version": "14.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://www.it-projects.info",
+    "license": "LGPL-3",
+    "depends": [
+        "auth_signup",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/auth_signup_login_templates.xml",
+    ],
+}
