01c8576cf76f5f1ec313a748aad7e000a4911240
Eugene Molotov <molotov@it-projects.info>
2023-09-01 10:09:02 +0500

    [ADD] tg_archive: создал модуль для хранения данных во внешних сервисах

---
+++ tg_archive/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Archive Tools for Tribal Gathering""",
+    "version": "10.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://www.it-projects.info",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+        "account_voucher",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": ["views.xml"],
+}
