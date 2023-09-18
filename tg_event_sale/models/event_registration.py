0f87554908d740f1861b13a5986ee9d190e65d73
Eugene Molotov <molotov@it-projects.info>
2023-09-18 09:39:49 +0500

    [FIX] tc_event_sale: добавил поле x_payment_is_done

--- tg_event_sale/models/event_registration.py
+++ tg_event_sale/models/event_registration.py
@@ -8,7 +8,7 @@ class EventRegistration(models.Model):
         "payment.transaction",
         string="Payment Transaction",
         compute="_compute_transactions",
-          mpute_sudo=True,
+        compute_sudo=True,
     )
     x_payment_reference_done = fields.Many2many(
         "payment.transaction",
@@ -16,8 +16,12 @@ class EventRegistration(models.Model):
         compute="_compute_transactions",
         compute_sudo=True,
     )
+    x_payment_is_done = fields.Boolean(
+        compute="_compute_transactions",
+        compute_sudo=True,
+    )
 
-    @api.depends("sale_order_id", "sale_order_id.state")
+    @api.depends("sale_order_id", "sale_order_id.state", "sale_order_id.amount_total")
     def _compute_transactions(self):
         Transactions = self.env["payment.transaction"]
         txs = Transactions.search([("sale_order_ids", "in", self.mapped("sale_order_id").ids)])
@@ -25,3 +29,5 @@ class EventRegistration(models.Model):
             so_txs = txs.filtered(lambda x: r.sale_order_id in x.sale_order_ids)
             r.x_payment_reference = so_txs
             r.x_payment_reference_done = so_txs.filtered(lambda x: x.state == "done")
+            paid_amount = sum(r.x_payment_reference_done.mapped('amount') + [0])
+            r.x_payment_is_done = paid_amount >= r.sale_order_id.amount_total
