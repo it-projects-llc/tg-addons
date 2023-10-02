fd4583b71d384634fa782973d925c9663e4d7829
Eugene Molotov <molotov@it-projects.info>
2023-10-02 14:47:31 +0500

    [ADD] tg_website_event_sale: модуль для клиента

---
+++ tg_website_event_sale/controllers/main.py
@@ -0,0 +1,11 @@
+from odoo.http import route, request
+from odoo.addons.website_event_sale.controllers.main import WebsiteEventSaleController
+
+
+class WebsiteEventSaleExtendController(WebsiteEventSaleController):
+    @route()
+    def registration_confirm(self, *args, **post):
+        order = request.website.sale_get_order(force_create=False)
+        if order:
+            order.sudo().unlink()
+        return super(WebsiteEventSaleController, self).registration_confirm(*args, **post)
