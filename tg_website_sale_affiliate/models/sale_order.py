cd4b4c0b9395df7faca531dc2fc07486773ed9e2
Eugene Molotov <molotov@it-projects.info>
2023-11-21 10:09:33 +0500

    fixup! fixup! [ADD] tg_website_sale_affiliate: модуль для кастомизации website_sale_affiliate

--- tg_website_sale_affiliate/models/sale_order.py
+++ tg_website_sale_affiliate/models/sale_order.py
@@ -4,10 +4,6 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.model
-    def create(self, vals):
-        record = super(SaleOrder, self).create(vals)
-        # TODO: не применяется
-        record.code_promo_program_id = record.affiliate_request_id.affiliate_id.code_promo_program_id
-        record.recompute_coupon_lines()
-        return record
+    def _get_applicable_no_code_promo_program(self):
+        res = self.affiliate_request_id.affiliate_id.code_promo_program_id
+        return res + super(SaleOrder, self)._get_applicable_no_code_promo_program()
