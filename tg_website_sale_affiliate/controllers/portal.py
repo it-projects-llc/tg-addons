44d524daad1afdf279c6502742397a544f46747b
Eugene Molotov <molotov@it-projects.info>
2023-12-12 15:05:47 +0500

    affi

--- tg_website_sale_affiliate/controllers/portal.py
+++ tg_website_sale_affiliate/controllers/portal.py
@@ -1,4 +1,3 @@
-from odoo.exceptions import AccessError, MissingError
 from odoo.http import request, route
 
 from odoo.addons.portal.controllers.portal import CustomerPortal
@@ -40,33 +39,3 @@ class CustomerPortal(CustomerPortal):
         )
 
         return request.render("tg_website_sale_affiliate.portal_my_affiliates", values)
-
-    @route(
-        "/my/affiliates/<int:affiliate_id>/orders",
-        type="http",
-        auth="user",
-        website=True,
-    )
-    def portal_my_affiliate_orders(self, affiliate_id, **kw):
-        try:
-            # TODO: add security rules
-            affiliate = self._document_check_access("sale.affiliate", affiliate_id)
-        except (AccessError, MissingError):
-            return request.redirect("/my")
-
-        values = self._prepare_portal_layout_values()
-
-        SaleOrder = request.env["sale.order"]
-        order_ids = affiliate._get_order_dict().get(affiliate.id, [])
-        domain = [("id", "in", order_ids)]
-
-        orders = SaleOrder.sudo().search(domain)
-        values.update(
-            {
-                "orders": orders,
-                "affiliate": affiliate,
-                "page_name": "affiliate",
-            }
-        )
-
-        return request.render("tg_website_sale_affiliate.portal_my_orders", values)
