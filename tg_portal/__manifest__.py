2aa5c86cb1ffd97d04df959199b2b140bede1ba9
Eugene Molotov <molotov@it-projects.info>
2024-01-08 16:00:42 +0500

    [ADD] tg_portal: модификация portal

---
+++ tg_portal/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": """Portal modifications for Tribal Gathering""",
+    "version": "14.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "portal",
+    ],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
