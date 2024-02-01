122492ef2926b6f98a8a6002da5393812ac39d81
Eugene Molotov <molotov@it-projects.info>
2024-02-01 16:32:26 +0500

    [ADD] tg_partner_identification: модификация оригинального partner_identification

---
+++ tg_partner_identification/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": """Partner Identification Numbers modifications for Tribal Gathering""",
+    "version": "14.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "partner_identification",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+    "qweb": [],
+}
