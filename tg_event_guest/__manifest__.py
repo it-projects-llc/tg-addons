bc0b06f19ee85f1b45c09b1baee0b9801ec6ea30
Eugene Molotov <molotov@it-projects.info>
2024-02-22 11:08:05 +0500

    [ADD] tc_event_guest: модуль для клиента

---
+++ tg_event_guest/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": """Event guests""",
+    "version": "14.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "auth_signup",
+        "event_sale",
+        "portal",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/mail_template_data.xml",
+        "views/event_guest_views.xml",
+        "views/portal_templates.xml",
+    ],
+}
