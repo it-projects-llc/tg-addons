4892855d8d265485d6a00dbd39ed8198121715be
Eugene Molotov <molotov@it-projects.info>
2024-09-16 10:47:46 +0500

    [ADD] tg_marketing: модуль для клиента

---
+++ tg_marketing/models/res_partner_marketing_answer.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+
+class PartnerMarketingAnswer(models.Model):
+    _name = "res.partner.marketing.answer"
+    _description = "Marketing answers"
+    _order = "sequence ASC, id ASC"
+
+    sequence = fields.Integer(default=10)
+    field = fields.Many2one(
+        "ir.model.fields",
+        domain="[('model', '=', 'res.partner'), ('name', 'like', 'marketing_')]",
+    )
+    answer = fields.Char()
+    is_canned = fields.Boolean()
