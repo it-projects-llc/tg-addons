3b8d8fda78af58afd81ce7cf3315593d5a98bb72
Eugene Molotov <molotov@it-projects.info>
2024-09-16 10:47:46 +0500

    [ADD] tg_marketing: модуль для клиента

---
+++ tg_marketing/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": """Marketing fields in Contact""",
+    "version": "14.0.0.2.2",
+    "author": "IT-Projects LLC",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "contacts",
+        "auth_signup",
+        "sales_team",
+        "website_event",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_views.xml",
+        "views/contact_views.xml",
+        "wizard/marketing_answer_merge_views.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
---
+++ tg_marketing/readme/DESCRIPTION.md
@@ -0,0 +1,5 @@
+Adds following marketing fields to contact form (tab Marketing fields):
+
+* How did you find about us?
+
+They are shown in frontend ticket form, when user is logged in.
