42bf1620d821fb847281b8e4e385afedb7ffbe07
Eugene Molotov <molotov@it-projects.info>
2024-09-16 10:47:46 +0500

    [ADD] tg_marketing: модуль для клиента

---
+++ tg_marketing/README.rst
@@ -0,0 +1,21 @@
+=============================
+ Marketing fields in Contact
+=============================
+
+Credits
+=======
+
+Contributors
+------------
+
+* `Eugene Molotov <https://github.com/em230418>`__
+
+Sponsors
+--------
+
+* `Tribal Gathering <https://www.tribalgathering.com/>`__
+
+Maintainers
+-----------
+
+* `IT-Projects LLC <https://it-projects.info>`__
---
+++ tg_marketing/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": """Marketing fields in Contact""",
+    "version": "14.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "contacts",
+        "auth_signup",
+        "website_event",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_views.xml",
+        "views/contact_views.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
