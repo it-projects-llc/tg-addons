92752d0f83938a75a4bc22370ca4c0c6c0be79b4
Eugene Molotov <molotov@it-projects.info>
2024-09-17 16:10:04 +0500

    [IMP] tg_base: pre-commit fixes

--- tg_base/wizard/base_partner_merge.py
+++ tg_base/wizard/base_partner_merge.py
@@ -2,9 +2,11 @@ from odoo import models
 
 
 class MergePartnerAutomatic(models.TransientModel):
-    _inherit = 'base.partner.merge.automatic.wizard'
+    _inherit = "base.partner.merge.automatic.wizard"
 
     def _merge(self, partner_ids, dst_partner=None, extra_checks=True):
         if self.env.user.has_group("account.group_account_user"):
             extra_checks = False
-        return super(MergePartnerAutomatic, self)._merge(partner_ids, dst_partner, extra_checks)
+        return super(MergePartnerAutomatic, self)._merge(
+            partner_ids, dst_partner, extra_checks
+        )
