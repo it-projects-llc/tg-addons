d1735d96fac89102913707bbae6887058b54cbb1
Eugene Molotov <molotov@it-projects.info>
2024-09-23 15:14:38 +0500

    [MIG] portal_passport_editing -> tg_portal: перенос всего функционала

--- tg_portal/models/res_partner.py
+++ tg_portal/models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class Partner(models.Model):
@@ -6,10 +6,29 @@ class Partner(models.Model):
 
     is_passport_not_ready = fields.Boolean()
 
+    passport = fields.Char(
+        string="Passport Number",
+        compute=lambda s: s._compute_identification(
+            "passport",
+            "passport",
+        ),
+        inverse=lambda s: s._inverse_identification(
+            "passport",
+            "passport",
+        ),
+        search=lambda s, *a: s._search_identification("passport", *a),
+    )
+
     cedula = fields.Char(
         string="Cedula Number",
-        compute=lambda s: s._compute_identification("cedula", "cedula",),
-        inverse=lambda s: s._inverse_identification("cedula", "cedula",),
+        compute=lambda s: s._compute_identification(
+            "cedula",
+            "cedula",
+        ),
+        inverse=lambda s: s._inverse_identification(
+            "cedula",
+            "cedula",
+        ),
         search=lambda s, *a: s._search_identification("cedula", *a),
     )
     has_cedula = fields.Boolean(
