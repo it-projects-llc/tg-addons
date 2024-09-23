831dd2cdf7abf5a80d9a987501727b2700f04651
Kuat Azimkhan <kuat.0111@gmail.com>
2024-09-23 15:15:56 +0500

    [MIG] tg_website_sale_affiliate: Migration to 17.0
    Co-authored-by: Eugene Molotov <em230418@users.noreply.github.com>

--- tg_website_sale_affiliate/models/__init__.py
+++ tg_website_sale_affiliate/models/__init__.py
@@ -1,3 +1,4 @@
 from . import sale_affiliate
 from . import sale_order
 from . import res_partner
+from . import loyalty_program
--- tg_website_sale_affiliate/models/sale_order.py
+++ tg_website_sale_affiliate/models/sale_order.py
@@ -1,9 +1,36 @@
-from odoo import models
+from odoo import _, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _get_applicable_no_code_promo_program(self):
-        res = self.affiliate_request_id.affiliate_id.code_promo_program_id
-        return res + super(SaleOrder, self)._get_applicable_no_code_promo_program()
+    def _program_check_compute_points(self, programs):
+        res = super()._program_check_compute_points(programs)
+        affiliate_promo_program = (
+            self.affiliate_request_id.affiliate_id.code_promo_program_id
+        )
+        for program in programs:
+            if program not in res:
+                # should not happen
+                continue
+
+            if "error" in res[program]:
+                continue
+
+            if program.only_affiliate_usage:
+                if program != affiliate_promo_program:
+                    res[program]["error"] = _("This should be applied via affiliate")
+
+        return res
+
+    def _try_apply_code(self, code):
+        self.ensure_one()
+
+        SaleAffiliate = self.sudo().env["sale.affiliate"]
+        affiliate = SaleAffiliate.search([("promo_code", "=", code)], limit=1)
+
+        if affiliate:
+            self.affiliate_request_id = affiliate.get_request()
+            return {}
+
+        return super()._try_apply_code(code)
