64aee28bbfb89afdb04a95f86a119275f7225f2b
Eugene Molotov <molotov@it-projects.info>
2024-09-23 15:14:38 +0500

    [IMP] portal_passport_editing: pre-commit + tidy up

--- portal_passport_editing/__manifest__.py
+++ portal_passport_editing/__manifest__.py
@@ -1,40 +1,22 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": """Passport Portal Editing""",
     "summary": """Allows portal users change passport information in portal""",
     "category": "Marketing",
-    # "live_test_url": "http://apps.it-projects.info/shop/product/portal-event-tickets?version=12.0",
     "images": ["images/banner.jpg"],
-    "version": "14.0.1.0.2",
-    "application": False,
-
-    "author": "IT-Projects LLC, Kolushov Alexandr",
-    "support": "apps@it-projects.info",
-    "website": "https://it-projects.info/team/yelizariev",
-    "license": "LGPL-3",
-    # "price": 9.00,
-    # "currency": "EUR",
-
+    "version": "17.0.1.0.2",
+    "author": "IT-Projects LLC",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "AGPL-3",
     "depends": [
-        "website_event_attendee_fields_custom",
         "portal",
+        "partner_contact_nationality",
+        "partner_identification",
+        "partner_contact_birthdate",
     ],
-    "external_dependencies": {"python": [], "bin": []},
     "data": [
         "views/assets.xml",
         "views/website_portal_templates.xml",
     ],
-    "qweb": [
-    ],
-    "demo": [
-    ],
-
-    "post_load": None,
-    "pre_init_hook": None,
-    "post_init_hook": None,
-
-    "auto_install": False,
-    "installable": True,
+    "demo": [],
 }
--- portal_passport_editing/controllers/controllers.py
+++ portal_passport_editing/controllers/controllers.py
@@ -1,26 +1,31 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+from odoo.http import request, route
 
 from odoo.addons.portal.controllers.portal import CustomerPortal, _
-from odoo.http import route, request
 
 
 class website_account_passport(CustomerPortal):
-
-    OPTIONAL_BILLING_FIELDS = CustomerPortal.OPTIONAL_BILLING_FIELDS + ["passport", "is_passport_not_ready", "birthdate_date", "nationality_id", "cedula", "has_cedula"]
+    OPTIONAL_BILLING_FIELDS = CustomerPortal.OPTIONAL_BILLING_FIELDS + [
+        "passport",
+        "is_passport_not_ready",
+        "birthdate_date",
+        "nationality_id",
+        "cedula",
+        "has_cedula",
+    ]
 
     @route()
     def account(self, redirect=None, **post):
         if post.get("nationality_id"):
             post["nationality_id"] = int(post["nationality_id"])
 
-        res = super(website_account_passport, self).account(redirect, **post)
-        res.qcontext["show_passport_checkbox"] = "event_guest" in request.env.user._fields
+        res = super().account(redirect, **post)
+        res.qcontext["show_passport_checkbox"] = (
+            "event_guest" in request.env.user._fields
+        )
         return res
 
     def details_form_validate(self, data):
-        res = super(website_account_passport, self).details_form_validate(data)
+        res = super().details_form_validate(data)
         for fname in ("is_passport_not_ready", "has_cedula"):
             data[fname] = data.get(fname, False)
 
@@ -32,9 +37,15 @@ class website_account_passport(CustomerPortal):
 
         is_something_extra_missing = False
         error, error_message = res
-        ignore_passport = bool(data["cedula"]) or request.env.user.has_group("base.group_user")
+        ignore_passport = bool(data["cedula"]) or request.env.user.has_group(
+            "base.group_user"
+        )
 
-        if not ignore_passport and not data["is_passport_not_ready"] and not data["passport"].strip():
+        if (
+            not ignore_passport
+            and not data["is_passport_not_ready"]
+            and not data["passport"].strip()
+        ):
             error["passport"] = "missing"
             is_something_extra_missing = True
 
@@ -55,7 +66,7 @@ class website_account_passport(CustomerPortal):
             is_something_extra_missing = True
 
         if is_something_extra_missing:
-            missing_error_message = _('Some required fields are empty.')
+            missing_error_message = _("Some required fields are empty.")
             if missing_error_message not in error_message:
                 error_message.append(missing_error_message)
 
