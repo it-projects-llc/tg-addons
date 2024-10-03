9aab5e545abd341341f1784044371c819982fd13
Kuat Azimkhan <kuat.0111@gmail.com>
2024-10-03 18:52:01 +0500

    [MIG] tg_auth_signup: Migration to 17.0

--- tg_auth_signup/models/res_partner.py
+++ tg_auth_signup/models/res_partner.py
@@ -8,18 +8,22 @@ class Partner(models.Model):
 
     @api.depends_context("website_id")
     def _compute_signup_url(self):
-        return super(Partner, self)._compute_signup_url()
+        return super()._compute_signup_url()
 
     def _get_signup_url_for_action(self, *args, **kw):
-        res = super(Partner, self)._get_signup_url_for_action(*args, **kw)
+        res = super()._get_signup_url_for_action(*args, **kw)
         if not self.env.context.get("relative_url"):
             website = self.env["website"].get_current_website()
             if website:
-                base_url = website._get_http_domain()
+                base_url = website.domain
 
                 if base_url:
                     for k in res.keys():
-                        relative_url = website._get_relative_url(res[k])
+                        relative_url = self._get_relative_url(res[k])
                         res[k] = werkzeug.urls.url_join(base_url, relative_url)
 
         return res
+
+    def _get_relative_url(self, url):
+        """Remove scheme and domain from the given URL, returning a relative URL."""
+        return werkzeug.urls.url_parse(url).replace(scheme="", netloc="").to_url()
