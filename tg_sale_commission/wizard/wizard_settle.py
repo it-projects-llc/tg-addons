2f5dfcc6b0b604f4551e8f9e8bc6cb6f39ff6991
Kuat Azimkhan <kuat.0111@gmail.com>
2024-10-09 15:06:52 +0500

    [IMP] tg_sale_commission: pre-commit auto fixes

--- tg_sale_commission/wizard/wizard_settle.py
+++ tg_sale_commission/wizard/wizard_settle.py
@@ -7,13 +7,9 @@ class SaleCommissionMakeSettle(models.TransientModel):
     def _get_settlement(self, agent, company, sett_from, sett_to):
         if company.commission_settlement_company:
             company = company.commission_settlement_company
-        return super(SaleCommissionMakeSettle, self)._get_settlement(
-            agent, company, sett_from, sett_to
-        )
+        return super()._get_settlement(agent, company, sett_from, sett_to)
 
     def _prepare_settlement_vals(self, agent, company, sett_from, sett_to):
         if company.commission_settlement_company:
             company = company.commission_settlement_company
-        return super(SaleCommissionMakeSettle, self)._prepare_settlement_vals(
-            agent, company, sett_from, sett_to
-        )
+        return super()._prepare_settlement_vals(agent, company, sett_from, sett_to)
