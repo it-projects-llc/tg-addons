a97aad51b56225cd2d65854c17b1b23b0ebb08e0
Kuat Azimkhan <kuat.0111@gmail.com>
2024-10-10 11:32:54 +0500

    [MIG] tg_sale_commission: Migration to 17.0

--- tg_sale_commission/wizard/wizard_settle.py
+++ tg_sale_commission/wizard/wizard_settle.py
@@ -2,12 +2,12 @@ from odoo import models
 
 
 class SaleCommissionMakeSettle(models.TransientModel):
-    _inherit = "sale.commission.make.settle"
+    _inherit = "commission.make.settle"
 
-    def _get_settlement(self, agent, company, sett_from, sett_to):
+    def _get_settlement(self, agent, company, currency, sett_from, sett_to):
         if company.commission_settlement_company:
             company = company.commission_settlement_company
-        return super()._get_settlement(agent, company, sett_from, sett_to)
+        return super()._get_settlement(agent, company, currency, sett_from, sett_to)
 
     def _prepare_settlement_vals(self, agent, company, sett_from, sett_to):
         if company.commission_settlement_company:
