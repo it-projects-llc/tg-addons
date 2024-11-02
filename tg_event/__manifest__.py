e43f227cd1dd478122def608b5a841162ad37d3e
Ilmir Karamov <ilmir.karamov@gmail.com>
2024-11-02 16:13:00 +0500

    [IMP] tg_event: badge templates for TG Lab
    Co-authored-by: Eugene Molotov <em230418@users.noreply.github.com>

--- tg_event/__manifest__.py
+++ tg_event/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": """Events Organization modifications for Tribal Gathering""",
-    "version": "14.0.0.1.0",
+    "version": "14.0.0.2.0",
     "author": "IT-Projects LLC, Eugene Molotov",
     "support": "it@it-projects.info",
     "website": "https://www.it-projects.info",
@@ -10,6 +10,8 @@
     ],
     "data": [
         "views/event_views.xml",
+        "reports/event_registration_badge_report.xml",
+        "reports/event_registration_template_tg_lab.xml",
     ],
     "demo": [],
     "qweb": [],
