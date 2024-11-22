deda799f089eff3323b6035f9c6416521bcd3e37
Eugene Molotov <molotov@it-projects.info>
2024-11-22 16:46:33 +0500

    [IMP] tg_event_guest: для существующих пользователей с привязанным гостем не надо посещать страницу с уникальным кодом

--- tg_event_guest/models/res_users.py
+++ tg_event_guest/models/res_users.py
@@ -7,12 +7,16 @@ class ResUsers(models.Model):
     event_guest = fields.Many2one("event.guest", compute="_compute_event_guest")
 
     def _compute_event_guest(self):
+        all_partner_ids = self.mapped("partner_id").ids
         event_guests = self.env["event.guest"].search(
             [
-                ("result_partner", "in", self.mapped("partner_id").ids),
-            ]
+                "|",
+                ("result_partner", "in", all_partner_ids),
+                ("guest_partner", "in", all_partner_ids),
+            ],
+            order="id ASC",
         )
-        d = {x.result_partner.id: x.id for x in event_guests}
+        d = {(x.result_partner or x.guest_partner).id: x.id for x in event_guests}
         for user in self:
             user.event_guest = d.get(user.partner_id.id, False)
 
