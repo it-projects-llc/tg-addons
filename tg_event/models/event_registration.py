eba63565e67fa00cdbc914b05e5ef26ec9d7fc39
azimkhankuat <kuat.0111@gmail.com>
2024-12-04 12:09:19 +0500

    [IMP] tg_event: pre-commit auto fixes

--- tg_event/models/event_registration.py
+++ tg_event/models/event_registration.py
@@ -6,7 +6,7 @@ class EventRegistration(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        records = super(EventRegistration, self).create(vals_list)
+        records = super().create(vals_list)
         for record in records:
             record.message_subscribe(record.event_id.subscribe_in_registrations.ids)
         return records
