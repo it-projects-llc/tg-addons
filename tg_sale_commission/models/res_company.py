afeb2a4656a9df0fa0b69517930d2085b92f5734
Eugene Molotov <molotov@it-projects.info>
2024-12-19 13:25:43 +0500

    [DOC] tg_sale_commission: добавили подсказку для поля commission settlement company

--- tg_sale_commission/models/res_company.py
+++ tg_sale_commission/models/res_company.py
@@ -5,5 +5,9 @@ class Company(models.Model):
     _inherit = "res.company"
 
     commission_settlement_company = fields.Many2one(
-        "res.company", domain="[('id', '!=', id), ('currency_id', '=', currency_id)]"
+        "res.company",
+        domain="[('id', '!=', id), ('currency_id', '=', currency_id)]",
+        help="If set, commission settlements will be autocorrected to "
+        "selected commission settlement company instead of company, "
+        "where associated affiliates are registered.",
     )
