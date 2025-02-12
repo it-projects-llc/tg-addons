553c63ca1033332c6423dd965ed4eb656005c677
Eugene Molotov <molotov@it-projects.info>
2025-02-12 09:14:32 +0500

    [ADD] tg_website_event_sale_commission: переводим слой интеграции с sale_commission в новый модуль

--- tg_website_event_sale/models/__init__.py
+++ tg_website_event_sale/models/__init__.py
@@ -1,4 +1,3 @@
 from . import sale_affiliate
-from . import sale_order_line
 from . import res_company
 from . import res_config_settings
---
+++ tg_website_event_sale_commission/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": """Online Event Ticketing Commissions customizations for Tribal Gathering""",  # noqa: E501
+    "version": "17.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "tg_sale_commission",
+        "tg_website_event_sale",
+    ],
+    "data": [],
+    "demo": [],
+}
