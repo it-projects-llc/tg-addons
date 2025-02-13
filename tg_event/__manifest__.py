1502f6f11b0d954f37222e433d0f1dbcb02f92a4
Ilmir Karamov <ilmir.karamov@gmail.com>
2025-02-13 18:20:37 +0500

    Revert "[IMP] change Download Tickets template on the confirmation page (#63)"
    This reverts commit 1477fd9076f8244cd24c800cf29241f45a6098e2.

--- tg_event/__manifest__.py
+++ tg_event/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": """Events Organization modifications for Tribal Gathering""",
-    "version": "17.0.0.4.0",
+    "version": "17.0.0.3.0",
     "author": "IT-Projects LLC, Eugene Molotov",
     "support": "it@it-projects.info",
     "website": "https://github.com/it-projects-llc/tg-addons",
