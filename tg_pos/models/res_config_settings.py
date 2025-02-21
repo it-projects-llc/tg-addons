29dbe9926ab055deab471abad9a257a862f7778a
Eugene Molotov <molotov@it-projects.info>
2025-02-21 17:03:50 +0500

    [IMP] tg_pos: возможность автоматического дублиррования инвойса

--- tg_pos/models/res_config_settings.py
+++ tg_pos/models/res_config_settings.py
@@ -7,3 +7,12 @@ class ResConfigSettings(models.TransientModel):
     pos_shop_ref_id = fields.Many2one(
         related="pos_config_id.shop_ref_id", readonly=False
     )
+    pos_auto_duplicate_invoices = fields.Boolean(
+        related="pos_config_id.auto_duplicate_invoices", readonly=False
+    )
+    pos_show_auto_duplicate_invoices = fields.Boolean(
+        related="pos_config_id.show_auto_duplicate_invoices", readonly=False
+    )
+    pos_debug_auto_duplicate_invoices = fields.Boolean(
+        related="pos_config_id.debug_auto_duplicate_invoices", readonly=False
+    )
