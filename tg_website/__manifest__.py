5761658c05a28ecc10162f3d483c81a83f227953
Eugene Molotov <molotov@it-projects.info>
2025-03-04 16:53:09 +0500

    [ADD] tg_website: terms and conditions (TNC)

---
+++ tg_website/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": """Website modifications for Tribal Gathering""",
+    "version": "17.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/auth_signup_login_templates.xml",
+        "views/website_views.xml",
+    ],
+    "demo": [],
+}
