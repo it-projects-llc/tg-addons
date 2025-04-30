5be3c19893c01999cc66996aeb92a7a0914f3d22
ILMIR <ilmir.karamov@gmail.com>
2025-04-30 16:26:11 +0500

    [NEW] tg_partner module (#76)
    * [NEW] tg_partner module

---
+++ tg_partner/README.rst
@@ -0,0 +1,23 @@
+======================================================
+ Partner / Contact modifications for Tribal Gathering
+======================================================
+
+* Added the fields to specify a group the contact belongs to, e.g. band, tribe and Nickname/Alias of the contact.
+
+Credits
+=======
+
+Contributors
+------------
+
+* `Ilmir Karamov <https://github.com/ilmir-k>`__
+
+Sponsors
+--------
+
+* `Tribal Gathering <https://www.tribalgathering.com/>`__
+
+Maintainers
+-----------
+
+* `IT-Projects LLC <https://it-projects.info>`__
---
+++ tg_partner/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": """Partner / Contact modifications for Tribal Gathering""",
+    "version": "17.0.1.0.0",
+    "author": "IT-Projects LLC, Ilmir Karamov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "demo": [],
+    "assets": {},
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+    "auto_install": False,
+    "installable": True,
+}
