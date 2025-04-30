c470cd8d6d0f12bc68487c48d9d6af75ca3c8602
Ilmir Karamov <ilmir.karamov@gmail.com>
2025-04-30 15:39:08 +0500

    [NEW] tg_partner module

---
+++ tg_partner/models/res_partner.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    group_name = fields.Char(
+        "Group / Band Name",
+        help="Specify a group the contact belongs to, e.g. band, tribe",
+    )
+
+    alias_name = fields.Char(
+        "Alias / Nickname",
+        help="Specify Nickname/Alias of the contact",
+    )
