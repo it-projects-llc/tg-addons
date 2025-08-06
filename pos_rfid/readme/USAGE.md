bf1dae42a50b655aa5716d2ea18f8286fc057b61
Eugene Molotov <molotov@it-projects.info>
2025-08-06 11:11:27 +0500

    [IMP] pos_rfid: добавил возможность чтения NFC-метод

--- pos_rfid/readme/USAGE.md
+++ pos_rfid/readme/USAGE.md
@@ -1,3 +1,14 @@
 * Open POS
 * Scan RFID
 * RESULT: POS reacts in the same way as if RFID was scanned via normal RFID reader
+
+NFC scanning
+------------
+
+* Make sure, NFC is available in your client device
+* Open POS
+* RESULT: You will see red NFC icon near avatar
+* Click on red NFC icon, enable NFC in your browser
+* RESULT: NFC icon is green
+* Put NFC-card under client device
+* RESULT: NFC is scanned as barcode
---
+++ pos_rfid/static/src/app/nfc_button/nfc_button.js
@@ -0,0 +1,44 @@
+/** @odoo-module **/
+
+import {Component, useState} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
+
+export class NFCButton extends Component {
+    static template = "pos_rfid.NFCButton";
+
+    setup() {
+        this.barcodeReader = useService("barcode_reader");
+        this.state = useState({
+            isRunning: false,
+        });
+        if (this.isVisible) {
+            setTimeout(() => this.startScanning(false), 500);
+        }
+    }
+
+    get isVisible() {
+        return "NDEFReader" in window;
+    }
+
+    async startScanning(onErrorRaise) {
+        const ndef = new window.NDEFReader();
+
+        ndef.addEventListener("reading", ({serialNumber}) => {
+            this.barcodeReader.scan(String(serialNumber.replaceAll(":", "")));
+        });
+
+        try {
+            await ndef.scan();
+        } catch (e) {
+            if (onErrorRaise) throw e;
+            return;
+        }
+
+        this.state.isRunning = true;
+    }
+
+    async click() {
+        if (this.state.isRunning) return;
+        await this.startScanning(true);
+    }
+}
