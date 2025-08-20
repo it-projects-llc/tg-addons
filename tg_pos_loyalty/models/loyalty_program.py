522f17fecb28a40de228635843c10fe58da804d3
Eugene Molotov <molotov@it-projects.info>
2025-08-20 11:58:37 +0500

    fixup! fixup! fixup! fixup! fixup! [ADD] tg_pos_loyalty: ввод счастливых часов

--- tg_pos_loyalty/models/loyalty_program.py
+++ tg_pos_loyalty/models/loyalty_program.py
@@ -7,13 +7,13 @@ class LoyaltyProgram(models.Model):
     are_happy_hours_enabled = fields.Boolean(
         compute="_compute_are_happy_hours_enabled", store=True, readonly=False
     )
-    happy_hours_monday = fields.Boolean("Monday")
-    happy_hours_tuesday = fields.Boolean("Tuesday")
-    happy_hours_wednesday = fields.Boolean("Wednesday")
-    happy_hours_thursday = fields.Boolean("Thursday")
-    happy_hours_friday = fields.Boolean("Friday")
-    happy_hours_saturday = fields.Boolean("Saturday")
-    happy_hours_sunday = fields.Boolean("Sunday")
+    happy_hours_monday = fields.Boolean("Mon")
+    happy_hours_tuesday = fields.Boolean("Tue")
+    happy_hours_wednesday = fields.Boolean("Wed")
+    happy_hours_thursday = fields.Boolean("Thu")
+    happy_hours_friday = fields.Boolean("Fri")
+    happy_hours_saturday = fields.Boolean("Sat")
+    happy_hours_sunday = fields.Boolean("Sun")
     happy_hours_from = fields.Float()
     happy_hours_to = fields.Float()
 
