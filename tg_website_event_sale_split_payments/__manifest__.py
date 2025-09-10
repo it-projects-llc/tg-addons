786f393fe88b7a2be1f15642cafdfd73ba120be3
Eugene Molotov <molotov@it-projects.info>
2025-09-10 11:39:42 +0500

    [IMP] tg_website_event_split_payments: увеличиваю версию для автоматического обновления

--- tg_website_event_sale_split_payments/__manifest__.py
+++ tg_website_event_sale_split_payments/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": """Split payments for Online ticketing""",
-    "version": "17.0.0.2.0",
+    "version": "17.0.0.3.0",
     "author": "IT-Projects LLC, Eugene Molotov",
     "support": "it@it-projects.info",
     "website": "https://github.com/it-projects-llc/tg-addons",
