ea3f12b208f0a9ff24d4e232f4be257634ab4aac
Eugene Molotov <52414052+em230418@users.noreply.github.com>
2026-02-19 12:07:29 +0500

    [ADD] tg_web: быстрофикс, чтобы отчеты работали (#104)

---
+++ tg_web/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": """Web modifications for Tribal Gathering""",
+    "version": "17.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/tg-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "web",
+    ],
+    "data": [],
+    "demo": [],
+}
