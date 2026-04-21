0ec764eb7f41fb0edd5892709c07a4aa1e45f5db
Ilmir Karamov <ilmir.karamov@gmail.com>
2026-04-21 18:47:18 +0500

    [IMP] tg_website_event: add website published info to event list view

--- tg_website_event/__manifest__.py
+++ tg_website_event/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": """Events modification for Tribal Gathering""",
-    "version": "17.0.0.2.0",
+    "version": "17.0.0.3.0",
     "author": "IT-Projects LLC, Eugene Molotov",
     "support": "it@it-projects.info",
     "website": "https://github.com/it-projects-llc/tg-addons",
