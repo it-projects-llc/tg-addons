bb1a56aaff303a61d2685a8c2d68ab0bcfe581d3
Eugene Molotov <52414052+em230418@users.noreply.github.com>
2026-05-06 15:06:24 +0500

    [FIX] *: followup https://github.com/odoo/odoo/pull/258967 (#133)

--- tg_auth_signup/controllers/main.py
+++ tg_auth_signup/controllers/main.py
@@ -2,8 +2,8 @@ from odoo.addons.auth_signup.controllers.main import AuthSignupHome
 
 
 class TGAuthSignupHome(AuthSignupHome):
-    def _prepare_signup_values(self, qcontext):
-        values = super()._prepare_signup_values(qcontext)
+    def _prepare_signup_values(self, qcontext, *args, **kw):
+        values = super()._prepare_signup_values(qcontext, *args, **kw)
         if "redirect" in qcontext and not qcontext.get("password"):
             values["redirect"] = qcontext["redirect"]
         return values
