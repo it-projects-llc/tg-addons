9e74efb4c6926d7728c1572e4cae5a560d565b1a
Eugene Molotov <molotov@it-projects.info>
2026-06-04 16:30:53 +0500

    fixup! fixup! fixup! [FIX] tg_website_sale_renting: исправил неправильное смещение default start и end date на фронтенде

--- tg_website_sale_renting/models/sale_order.py
+++ tg_website_sale_renting/models/sale_order.py
@@ -7,6 +7,9 @@ class SaleOrder(models.Model):
     def _has_product_specific_renting_period(self):
         products = self.mapped("order_line.product_template_id")
         for product in products:
+            if not product.rent_ok:
+                continue
+
             if product.renting_min_start_date or product.renting_max_date:
                 return True
 
