2c378306fabbbfc0971920ca64cee9295f859e58
Eugene Molotov <molotov@it-projects.info>
2026-06-09 13:10:41 +0500

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [FIX] tg_website_sale_renting: исправил неправильное смещение default start и end date на фронтенде

--- tg_website_sale_renting/post_load.py
+++ tg_website_sale_renting/post_load.py
@@ -29,3 +29,19 @@ def post_load():
         )
 
     SaleOrder._is_valid_renting_dates = _is_valid_renting_dates
+
+    from odoo.addons.website_sale_stock_renting.models.sale_order import (  # pylint: disable=reimported
+        SaleOrder,
+    )
+
+    def _is_valid_renting_dates(self):
+        """ Override to take into account the preparation time."""
+        res = super()._is_valid_renting_dates()
+        rental_order_lines = self.order_line.filtered('reservation_begin')
+        if not rental_order_lines or not res:
+            return res
+        max_padding_time = max(rental_order_lines.product_id.mapped('preparation_time'), default=0)
+        initial_time = self.rental_start_date - timedelta(hours=max_padding_time)
+        return initial_time >= fields.Datetime.now() - timedelta(days=1)   # <--- changes here
+
+    SaleOrder._is_valid_renting_dates = _is_valid_renting_dates
