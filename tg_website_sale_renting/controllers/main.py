04574268586277fabae00eb4002ba704aa831e31
Eugene Molotov <molotov@it-projects.info>
2026-06-18 14:35:47 +0500

    fixup! fixup! Revert "fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [FIX] tg_website_sale_renting: исправил неправильное смещение default start и end date на фронтенде"

--- tg_website_sale_renting/controllers/main.py
+++ tg_website_sale_renting/controllers/main.py
@@ -1,7 +1,6 @@
 from odoo.addons.website_sale_renting.controllers.main import (
     WebsiteSaleRenting,
     request,
-    route,
 )
 
 
@@ -10,17 +9,4 @@ class TGWebsiteSaleRenting(WebsiteSaleRenting):
         res = super()._prepare_product_values(product, *args, **kwargs)
         order_sudo = request.website.sale_get_order()
         res["deny_rent_this_product"] = not order_sudo._can_rent_this_product(product)
-        return res
-
-    @route()
-    def cart(self, *args, **kw):
-        res = super().cart(*args, **kw)
-        order = res.qcontext.get("website_sale_order")
-        if order and res.qcontext.get("suggested_products"):
-            res.qcontext["suggested_products"] = list(
-                filter(
-                    lambda x: not x.rent_ok or order._can_rent_this_product(x),
-                    res.qcontext["suggested_products"],
-                )
-            )
         return res
--- tg_website_sale_renting/post_load.py
+++ tg_website_sale_renting/post_load.py
@@ -1,3 +1,5 @@
+# ruff: noqa: B023
+
 def post_load():
     from odoo.addons.website_sale_renting.models.sale_order import (
         SaleOrder,
@@ -29,6 +31,35 @@ def post_load():
         )
 
     SaleOrder._is_valid_renting_dates = _is_valid_renting_dates
+
+    from odoo.addons.website_sale.models.sale_order import (  # pylint: disable=reimported
+        SaleOrder,
+        random,
+    )
+
+    def _cart_accessories(self):
+        """ Suggest accessories based on 'Accessory Products' of products in cart """
+        product_ids = set(self.website_order_line.product_id.ids)
+        all_accessory_products = self.env['product.product']
+        for line in self.website_order_line.filtered('product_id'):
+            accessory_products = line.product_id.product_tmpl_id._get_website_accessory_product()
+            accessory_products = accessory_products.filtered(lambda x, order=line.order_id: not x.rent_ok or order._can_rent_this_product(x))  # <--- added this
+            if accessory_products:
+                # Do not read ptavs if there is no accessory products to filter
+                combination = line.product_id.product_template_attribute_value_ids + line.product_no_variant_attribute_value_ids
+                all_accessory_products |= accessory_products.filtered(lambda product:
+                    product.id not in product_ids
+                    and product.filtered_domain(self.env['product.product']._check_company_domain(line.company_id))
+                    and product._is_variant_possible(parent_combination=combination)
+                    and (
+                        not self.website_id.prevent_zero_price_sale
+                        or product._get_contextual_price()
+                    )
+                )
+
+        return random.sample(all_accessory_products, len(all_accessory_products))
+
+    SaleOrder._cart_accessories = _cart_accessories
 
     from odoo.addons.website_sale_stock_renting.models.sale_order import (  # pylint: disable=reimported
         SaleOrder,
