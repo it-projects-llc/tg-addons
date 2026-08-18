826b23af6f6bbd864cd1a5cf6517a8469f2ec96c
Eugene Molotov <molotov@it-projects.info>
2026-08-18 10:08:23 +0500

    [IMP] tg_website_sale: добавили возможность просмотреть список sale-журналов к вебсайту

--- tg_website_sale/models/website.py
+++ tg_website_sale/models/website.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import _, models
 
 
 class Website(models.Model):
@@ -6,3 +6,22 @@ class Website(models.Model):
 
     def _display_partner_b2b_fields(self):
         return False
+
+    # based on account.move._search_default_journal
+    def _action_show_sale_journals(self):
+        self.ensure_one()
+
+        journal_types = ["sale"]
+        company = self.company_id
+        domain = [
+            *self.env["account.journal"]._check_company_domain(company),
+            ("type", "in", journal_types),
+        ]
+
+        return {
+            "name": _("Journals"),
+            "type": "ir.actions.act_window",
+            "view_mode": "tree,form",
+            "res_model": "account.journal",
+            "domain": domain,
+        }
